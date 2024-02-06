cb10fa113bcdd3ea0042651a86f11a6b2fbfe202
Cíntia Brito <cintiahsb@hotmail.com>
2024-02-05 21:44:32 -0300

    preenchendo pastas

---
+++ src/repository/FilmesRepository.java
@@ -0,0 +1,11 @@
+public interface FilmesRepository{
+
+    public Filme inserir(Filme filme);
+
+    public Filme atualizar(Filme filme);
+
+    public void excluir(Filme filme);
+
+    public List<Filme> pesquisarPorNome(String nome);
+
+}
