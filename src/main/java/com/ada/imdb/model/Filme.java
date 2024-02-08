30cf796093a107c57b04ab937948ae49757d2d85
Moises Almeida <moisesalmeida.dev@gmail.com>
2024-02-07 21:02:59 -0400

    Criando classe static repository de filmes

--- src/main/java/com/ada/imdb/model/Filme.java
+++ src/main/java/com/ada/imdb/model/Filme.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Filme {
+    private int id;
     private String nome;
     private String genero;
     private String ano;
@@ -12,4 +13,32 @@ public class Filme {
     private String sinopse;
     List<Ator> atores = new ArrayList<>();
     List<Diretor> diretores = new ArrayList<>();
+
+    public Filme(String nome) {
+        this.nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Filme{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
